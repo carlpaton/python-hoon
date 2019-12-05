7cc2db5e2fa69d3ce23565693430e816108e860e
Carl <carl.paton@gmail.com>
2019-12-05 22:01:39 +1300

    3 python basics/17 Numbers

---
+++ complete-python-developer-zero-to-mastery/3 Python Basics/17. Numbers.py
@@ -0,0 +1,25 @@
+# Simple math operations
+print('--------- simple math operations ---------')
+print(2 + 4)
+print(2 - 4)
+print(2 * 4)
+print(2 / 4)
+
+# Type action
+print('--------- type action ---------')
+print(type(2 / 4))
+
+# Power of
+print('--------- power of ---------')
+print(2 ** 6)
+
+# Devide rounded down
+# Will cast the result to an int
+print('--------- devide rounded down ---------')
+print(2 // 4) # returns 0
+print(5 // 4) # returns 1
+
+# Modulo
+# Calculates the remainder of this division
+print('--------- modus ---------')
+print(5 % 2) # returns 1
