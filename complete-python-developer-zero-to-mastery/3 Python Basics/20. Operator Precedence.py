991ec7cadf82f73e9776d5922ae4992867435491
Carl <carl.paton@gmail.com>
2019-12-06 15:14:02 +1300

    math fuctions, operator precedence, more types!

---
+++ complete-python-developer-zero-to-mastery/3 Python Basics/20. Operator Precedence.py
@@ -0,0 +1,23 @@
+# Operator precedence
+print((20 - 3) + 2 ** 2)
+
+
+# precedence is
+# ()
+# **
+# * /
+# + -
+
+
+# https://repl.it/@aneagoie/Operator-Precedence
+print((5 + 4) * 10 / 2)      # 45.0
+print(((5 + 4) * 10) / 2)    # 45.0
+print((5 + 4) * (10 / 2))    # 45.0
+print(5 + (4 * 10) / 2)      # 25.0
+print(5 + 4 * 10 // 2)       # 25
+
+
+
+
+
+
---
+++ complete-python-developer-zero-to-mastery/3 Python Basics/22. Optional bin() and complex.py
@@ -0,0 +1,8 @@
+# complex data type
+# https://docs.python.org/3.8/c-api/complex.html
+
+# bin
+print(bin(5))  #0b101
+
+# convert bin to int
+print(int('0b101', 2))
